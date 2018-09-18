13d9587c71d778ed17f9c4c2955b973636f1c289
Ramil Sitdikov <sitdikov.ramil@gmail.com>
2018-09-18 23:33:32 +0300

    Say bye

--- bot.rb
+++ bot.rb
@@ -9,7 +9,10 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
+      text = "id: #{message.from.id}, first_name: #{message.from.first_name}"
+      bot.api.send_message(chat_id: message.chat.id, text: text)
+    when '/add_chat'
+      id = message.from.first_name.id
     when '/end'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
     else
