55a8309da858ef46935e2eadd1b20c5e1f4ce903
Ramil Sitdikov <sitdikov.ramil@gmail.com>
2018-09-20 00:47:51 +0300

    Check message full info

--- models/tg_bot.rb
+++ models/tg_bot.rb
@@ -6,16 +6,19 @@ class TgBot
 
   def process_message
     bot.listen do |message|
+      chat_id = message.chat.id
+
       case message.text
       when '/start'
         text = "id: #{message.from.id}, first_name: #{message.from.first_name}"
-        bot.api.send_message(chat_id: message.chat.id, text: text)
-      when '/add_chat'
-        id = message.from.first_name.id
+        bot.api.send_message(chat_id: chat_id, text: text)
+      when '/message'
+        text = "message: #{message.inspect}"
+        bot.api.send_message(chat_id: chat_id, text: text)
       when '/end'
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
+        bot.api.send_message(chat_id: chat_id, text: "Bye, #{message.from.first_name}!")
       else
-        bot.api.send_message(chat_id: message.chat.id, text: "I don't understand you :(")
+        bot.api.send_message(chat_id: chat_id, text: "I don't understand you.")
       end
     end
   end
